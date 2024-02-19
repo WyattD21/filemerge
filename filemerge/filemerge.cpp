3376bacaea1aca3b241eb1de6fc8c1e74376521e
WyattD21 <157396023+WyattD21@users.noreply.github.com>
2024-02-19 04:12:27 -0500

    Update filemerge.cpp

--- filemerge/filemerge.cpp
+++ filemerge/filemerge.cpp
@@ -10,13 +10,11 @@ int main() {
 
     string line1, line2;
 
-    // Read initial lines from both files
     getline(input1, line1);
     getline(input2, line2);
 
-    // Merge files based on the key field
     while (!input1.eof() && !input2.eof()) {
-        string key1 = line1.substr(0, line1.find(' ')); // Assuming space-separated key field
+        string key1 = line1.substr(0, line1.find(' ')); 
         string key2 = line2.substr(0, line2.find(' '));
 
         if (key1 < key2) {
@@ -29,19 +27,16 @@ int main() {
         }
     }
 
-    // Write remaining lines from input1
     while (!input1.eof()) {
         output << line1 << endl;
         getline(input1, line1);
     }
 
-    // Write remaining lines from input2
     while (!input2.eof()) {
         output << line2 << endl;
         getline(input2, line2);
     }
 
-    // Close file streams
     input1.close();
     input2.close();
     output.close();
